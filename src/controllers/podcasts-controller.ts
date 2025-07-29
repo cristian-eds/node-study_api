c8bf8d85b27c54bbff3aea8194dc8799944ff2ab
Cristian <cristian.santos0308@gmail.com>
2025-07-29 09:59:17 -0300

    Created episodes-service

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,8 +1,9 @@
-import {IncomingMessage, ServerResponse } from 'http';
+import { IncomingMessage, ServerResponse } from 'http';
+import {listEpisodes} from '../services/episodes-service'
 
 export const getEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
-    res.writeHead(200, {'Content-Type': "application/json"});
-    res.end(JSON.stringify({
-        name: "Teste"
-    }))
+    const content = await listEpisodes();
+    
+    res.writeHead(200, { 'Content-Type': "application/json" });
+    res.end(JSON.stringify(content))
 }
---
+++ src/services/episodes-service.ts
@@ -0,0 +1,14 @@
+
+
+export const listEpisodes = async () => {
+    const data = [
+        {
+            podcastName: "flow",
+            episode: "Teste - #319",
+            videoId: "pQSuQmUS530",
+            categories: ["saude", "esporte", "bodybuilder"],
+        }
+    ]
+
+    return data;
+}
